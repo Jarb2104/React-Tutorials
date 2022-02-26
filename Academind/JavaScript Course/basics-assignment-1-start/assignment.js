b2e192fda1158efb79f3e63dc0a2baf3ea3ba893
Alejandro Reyes <Jarb2104@hotmail.com>
2022-02-26 11:49:48 -0600

    Working with variables in Java Script End.

---
+++ Academind/JavaScript Course/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const myVarJuan = 13;
+let myVarTu;
+
+myVarTu = 18 + myVarJuan;
+
+myVarTu = myVarTu - 18;
+myVarTu = myVarTu * 3;
+myVarTu = myVarTu / 3;
+
+alert(myVarJuan);
+alert(myVarTu);
---
+++ Academind/JavaScript Course/javascript_project_course/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const initialVal = 0;
+let calcResult = initialVal;
+
+calcResult = (calcResult + 13) * 3 / 2 - 1;
+let calDescription = `(${initialVal} + 13) * 3 / 2 - 1`;
+
+outputResult(calcResult, calDescription);
